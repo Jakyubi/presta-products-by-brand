f5f523d1ab3b2441ecce41305ee0aba3d4156f46
Jakyubi <99651126+Jakyubi@users.noreply.github.com>
2025-08-13 13:13:41 +0200

    add constructor, install, uninstall

--- modules/productsbybrand/productsbybrand.php
+++ modules/productsbybrand/productsbybrand.php
@@ -5,6 +5,47 @@ if(!defined('_PS_VERSION_')){
 
 class ProductsByBrand extends Module
 { 
+    public function __construct()
+    {
+        $this->name = 'productsbybrand';
+        $this->tab = 'front_office_features';
+        $this->author = 'abc';
+        $this->need_instance = 0;
+        $this->ps_versions_compliancy = [
+            'min' => '8.0.0',
+            'max' => _PS_VERSION_,
+        ];
+        $this->bootstrap = true;
+
+        parent::__construct();
+
+        $this->displayName = $this->trans('Products by brand', [], 'Modules.Productsbybrand.Admin');
+        $this->description = $this->trans('This module lets you search for products of each brand', [], 'Modules.Productsbybrand.Admin');
+        $this->confirmUninstall = $this->trans('Are you sure to uninstall?', [], 'Modules.Productsbybrand.Admin');
+
+        if(!Configuration::get('PRODUCTSBYBRAND_MODULE_NAME')){
+            $this->warning = $this->trans('No name provided', [], 'Modules.Productsbybrand.Admin');
+        }
+
+    }
+
+    public function install()
+    {
+        return(
+            parent::install()
+            && Configuration::updateValue('PRODUCTSBYBRAND_MODULE_NAME', 'Products by name')
+        );
+
+    }
+
+    public function uninstall()
+    {
+        return (
+            parent::uninstall()
+            && Configuration::deleteByName('PRODUCTSBYBRAND_MODULE_NAME')
+        );
+
+    }
 
     
 }
