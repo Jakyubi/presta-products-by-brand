076737ad02ce5160856922f74947a120c7a9cd7f
Jakyubi <99651126+Jakyubi@users.noreply.github.com>
2025-08-19 15:17:47 +0200

    add admin config panel on off

--- modules/productsbybrand/productsbybrand.php
+++ modules/productsbybrand/productsbybrand.php
@@ -66,6 +66,11 @@ class ProductsByBrand extends Module
 
     public function hookDisplayHome($params)
     {
+        $enabled = (bool) Configuration::get('PRODUCTSBYBRAND_MODULE_ENABLE');
+        if(!$enabled){
+            return '';
+        }
+
         $brands = Manufacturer::getManufacturers(false, $this->context->language->id ,false);
         $grouped = [];
 
@@ -84,6 +89,76 @@ class ProductsByBrand extends Module
         return $this->display(__FILE__, 'views/templates/hook/templateFront.tpl');
 
     }
+
+
+    public function getContent()
+    {
+        if(Tools::isSubmit('submitProductsbybrandSettingsForm')){
+            $enabled = Tools::getValue('PRODUCTSBYBRAND_MODULE_ENABLE');
+
+            Configuration::updateValue('PRODUCTSBYBRAND_MODULE_ENABLE', $enabled);
+        }
+
+        $this->context->smarty->assign([
+            'displayForm' => $this->displayForm(),
+        ]);
+
+        return $this->display(__FILE__, 'views/templates/admin/configure.tpl');
+    }
+
+    public function displayForm()
+    {
+        $form = [
+            'form' => [
+                'legend' => [
+                    'title' => $this->l('Settings'),
+                ],
+                'input' => [
+                    [
+                        'type' => 'switch',
+                        'label' => $this->l('Enable module'),
+                        'name' => 'PRODUCTSBYBRAND_MODULE_ENABLE',
+                        'is_bool' => true,
+                        'values' => [
+                            [
+                                'id' => 'active_on',
+                                'value' => 1,
+                                'label' => $this->l('Enabled')
+                            ],
+                            [
+                                'id' => 'active_off',
+                                'value' => 0,
+                                'label' => $this->l('Disabled')
+                            ],
+                        ],
+                    ],
+                ],
+                'submit' => [
+                    'title' => $this->l('Save'),
+                    'class' => 'btn btn-default pull-right',
+                    'name' => 'submitProductsbybrandSettingsForm',
+                ],
+            ],
+        ];
+
+
+        $helper = new HelperForm();
+        
+        $helper->table = $this->table;
+        $helper->name_controller = $this->name;
+        $helper->token = Tools::getAdminTokenLite('AdminModules');
+        $helper->currentIndex = AdminController::$currentIndex . '&' . http_build_query(['configure' => $this->name]);
+        $helper->submit_action = 'submitProductsbybrandSettingsForm';
+        $helper->default_form_language = (int) Configuration::get('PS_LANG_DEFAULT');
+
+        $helper->fields_value['PRODUCTSBYBRAND_MODULE_ENABLE'] = 
+        Tools::getValue('PRODUCTSBYBRAND_MODULE_ENABLE', Configuration::get('PRODUCTSBYBRAND_MODULE_ENABLE'));
+
+        return $helper->generateForm([$form]);
+
+    }
+
+
 }
 
 
